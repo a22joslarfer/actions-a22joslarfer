464eb4a56ea852c4dac98e4ea8f1ccaa6070525b
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-02-28 09:11:59 +0100

    li

--- web/actions-vue/src/components/Number.vue
+++ web/actions-vue/src/components/Number.vue
@@ -1,34 +1,19 @@
-<!-- Number.vue -->
-
 <template>
   <div>
-    <button v-for="number in numbers" :key="number" @click="handleClick(number)" :class="{ 'clicked': clickedNumbers.includes(number) }">
-      {{ number }}
-    </button>
+    <ul>
+      <li v-for="number in 9" :key="number" @click="showAlert(number)">
+        {{ number }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const clickedNumbers = ref([]);
-
-const handleClick = (number) => {
-  if (!clickedNumbers.value.includes(number)) {
-    clickedNumbers.value.push(number);
-  } else {
-    // Si el número ya está clicado, no hacemos nada
+<script>
+export default {
+  methods: {
+    showAlert(number) {
+      alert(`You selected ${number}`);
+    }
   }
-
-  // Mostrar los números clicados en la consola
-  console.log('Números clicados:', clickedNumbers.value);
-};
-</script>
-
-<style scoped>
-button {
-  margin: 0.5rem;
-  padding: 0.5rem 1rem;
 }
-</style>
+</script>
